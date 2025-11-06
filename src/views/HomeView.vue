88cf82c260c463b15f0377825c80b2cc885e171e
MCPavanello <pavanelloclaraa@gmail.com>
2025-11-06 11:56:37 -0300

    FEAT: Começando header. #17

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,16 @@
 <script setup>
 </script>
 <template>
-    <h1>home</h1>
+    <div>
+        <h2>Explore o LenteLatina e conecte-se a comunidade de produções audiovisioais latinoamericanas</h2>
+</div>
 </template>
+<style>
+
+h2{
+    margin-bottom: 2vw;
+    font-size: 1.5vw;
+    text-align: center;
+}
+
+</style>

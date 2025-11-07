893e96d542326757ffce9d25e3db9928a9422743
PedGabriel <pedrogabrielgoncalves91@gmail.com>
2025-11-07 14:40:59 -0300

    FEAT: Store de gêneros adicionada e funcional

--- src/utils/textes.vue
+++ src/utils/textes.vue
@@ -1,21 +1,33 @@
 <script setup>
 import { onMounted } from 'vue';
 import { useCountryStore } from '@/stores/paises';
+import { useGeneroStore } from '@/stores/genero';
 
 const countryStore = useCountryStore();
+const generoStore = useGeneroStore();
 
 onMounted(async () =>{
     await countryStore.getAllCountrys()
+    await generoStore.getAllGeneros('movie')
 })
 </script>
 <template>
-    <h3>Países</h3>
+    <h3>Países latinos:</h3>
     <ul>
         <li
         v-for="country in countryStore.latinCountries"
         :key="country.id"
         >
         {{ country.native_name }}
+        </li>
+    </ul>
+    <br>
+    <h3>Gêneros:</h3>
+    <ul>
+        <li
+        v-for="genero in generoStore.generos" :key="genero.id"
+        >
+        {{ genero.name }}
         </li>
     </ul>
 </template>

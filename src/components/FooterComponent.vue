422f16a134152d578ac7d6279060ed1105804ca6
MCPavanello <pavanelloclaraa@gmail.com>
2025-11-27 11:55:02 -0300

    FEAT: Começando a mudança do header.

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -5,56 +5,30 @@
 
 <template>
 <footer>
-    <div class="direitos">
-        <h4>2025 &copy; LenteLatina</h4>
-    </div>
-    <div class="redes">
-        <h4>Redes Sociais:</h4>
-        <ul>
-            <li><span class="mdi mdi-instagram"></span> </li>
-            <li><span class="mdi mdi-twitter"></span></li>
-            <li><span class="mdi mdi-facebook"></span></li>
-        </ul>
-    </div>
-    <div class="projetado">
-        <h4>Projeto por:</h4> 
+    <div class="dev">
+        <h4>Desenvolvedores:</h4> 
         <ul>
             <li> Pedro Gabriel Gonçalves</li>
             <li> Maria Clara Pavanello Giareta</li>
         </ul>
     </div>
-    
+    <div class="redes">
+            <h4>Redes Sociais</h4>
+                  <ul class="icons">
+        <li><span class="mdi mdi-facebook"></span></li>
+        <li><span class="mdi mdi-instagram"></span></li>
+        <li><span class="mdi mdi-twitter"></span></li>
+      </ul>
+
+            <p>LenteLatina © 2025</p>
+    </div>
 </footer>
 </template>
 <style scoped>
-footer {
+footer{
+    background-color: #0D1321;
+    color: #F9FADC;
     display: flex;
-    justify-content: space-between;
-    background-color: #69140E;
-    color: #FCFCED;
-    padding: 3vw 6vw;
-    align-items: center;
-    
-}
-div{
-    margin: 0 5vw 0 5vw;
-}
-div li span{
-    color: #FCFCED;
-    font-size: 1vw;
-    
-}
-div.redes ul{
-    display: flex;
-}
-div.redes ul li{
-    margin-left: 1vw;
-}
-h4{
-    margin-bottom: 1vw;
-}
-div.projetado li{
-    margin-bottom: 0.3vw;
 }
 
 

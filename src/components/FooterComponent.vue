5c0d7e798a5b7d7f9134e72faf4c9b382965e5bf
MCPavanello <pavanelloclaraa@gmail.com>
2025-11-28 10:56:17 -0300

    FEAT: Concluindo o footer e adionando linha na parte inferior do header.

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -8,7 +8,7 @@
     <div class="dev">
         <h4>Desenvolvedores:</h4> 
         <ul>
-            <li> Pedro Gabriel Gonçalves</li>
+            <li class="prikito"> Pedro Gabriel Gonçalves</li>
             <li> Maria Clara Pavanello Giareta</li>
         </ul>
     </div>
@@ -19,9 +19,10 @@
         <li><span class="mdi mdi-instagram"></span></li>
         <li><span class="mdi mdi-twitter"></span></li>
       </ul>
-
-            <p>LenteLatina © 2025</p>
+  <p>LenteLatina © 2025</p>
+           
     </div>
+   
 </footer>
 </template>
 <style scoped>
@@ -29,7 +30,33 @@ footer{
     background-color: #0D1321;
     color: #F9FADC;
     display: flex;
+     justify-content: space-between;
+    padding: 3vw 10vw;
+    position: relative;
+
 }
-
-
+footer::after{
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 1px;
+    top: 0.5rem;
+    left: 0;
+    background-color: #F9FADC;
+}
+.redes{
+    text-align: center;
+}
+.redes ul{
+    display: flex;
+    justify-content: center;
+    margin-top: 1vw;
+}
+.redes ul{
+    gap: 1.5vw;
+    margin-bottom: 1vw;
+}
+li.prikito{
+    margin: 1vw 0 0.5vw 0;
+}
 </style>
